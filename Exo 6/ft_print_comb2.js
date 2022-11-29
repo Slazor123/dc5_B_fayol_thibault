dd8ffe1aca2c1be5c1502ae2d2de363f7ce1345e
Thibault F <101000352+Slazor123@users.noreply.github.com>
2022-11-29 16:10:51 +0100

    Add files via upload

--- Exo 6/ft_print_comb2.js
+++ Exo 6/ft_print_comb2.js
@@ -1,8 +1,9 @@
-function combinaisons() {
-  for (let i = 0; i < 10; i) {
-    for (let j = i + 1; j < 10; j){
-        console.log(i, j, k);
-      }
+function afficherCombinaisons() {
+  for (let i = 0; i < 10; i++) {
+    for (let j = i + 1; j < 10; j++) {
+      console.log(i + "00 " + j);
     }
   }
-combinaisons();
+}
+
+afficherCombinaisons();
